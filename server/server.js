4045996268da281b0209471d9708e19d6bd0b77e
Agey <pzyktzle@gmail.com>
2018-09-01 17:17:48 +1000

    Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,16 @@ var app = express();
 
 app.use(bodyParser.json());
 
+// GET /todos
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
+
+// POST /todos
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
